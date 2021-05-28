d2a1118057c2e1402ef78891bbd722a1d62d373f
Shahjada87 <71275176+Shahjada87@users.noreply.github.com>
2021-05-28 23:11:54 +0530

    Added Envioronmentsl variables to .env file

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -24,8 +26,7 @@ const userSchema = new mongoose.Schema({
   password : String
 });
 
-const secret = "this is a secret";
-userSchema.plugin(encrypt, {secret : secret, encryptedFields: ['password']});
+userSchema.plugin(encrypt, {secret : process.env.SECRET, encryptedFields: ['password']});
 
 const User = new mongoose.model("User", userSchema);
 
